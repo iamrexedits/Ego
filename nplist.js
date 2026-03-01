57fa336a91c1c413cd3ec136e685b26fe901611b
REX <ffconfig.op@gmail.com>
2026-03-01 11:58:52 +0600

    Add user ID list for no-prefix commands

---
+++ nplist.js
@@ -0,0 +1,7 @@
+// Users who can use no-prefix commands (by User ID)
+module.exports = [
+    'YOUR_USER_ID',      // Bot Owner
+    'ADMIN_USER_ID_1',   // Admin 1
+    'ADMIN_USER_ID_2',   // Admin 2
+    // Add more user IDs as needed
+];
